19d30f0047161f6516d8b649ac4d2943bd29776b
cheadevit <ch-devit@global-mobility-service.com>
2020-10-04 20:35:53 +0700

    Right link list articles

--- resources/views/contents/right_link.blade.php
+++ resources/views/contents/right_link.blade.php
@@ -80,93 +80,21 @@ img {
       <div class="posts_item right-hot-news">
     <div class="">
       <div class="title">អត្ថបទថ្មី<div class="border"></div></div>
-                                                            <div class="media">
-            <div class="media-left right-small-post item">
+      @foreach(\App\Models\Article::where('status', '1')->orderBy('amount_viewer', 'desc')->limit(10)->get() as $menu_item)
+        <div class="media">
+            {{-- <div class="media-left right-small-post item">
               <a href="https://news.sabay.com.kh/article/1231608#utm_campaign=onpage">
                 <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/Football(32)/foo/5f79328b875d1_1601778300_extra_small.jpg" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/Football(32)/foo/5f79328b875d1_1601778300_extra_small.jpg&quot;);"></div>
               </a>
-            </div>
+            </div> --}}
             <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231608#utm_campaign=onpage">
-                Pep ​ថា​លេង​ទើប​បាន​៣ ៤​ប្រកួត​ប្រកួត ​កុំ​និយាយ​រឿង​ពាន​រង្វាន់​អី​
+            <a href="{{ url('view/article/detail/'. $menu_item->id) }}">
+                <p>{{ $menu_item->title }}</p>
               </a>
             </div>
           </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231605#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/Kley-Kley/Phy/Week5-September-2020/5f7927e2257f3_1601775540_extra_small.jpg" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/Kley-Kley/Phy/Week5-September-2020/5f7927e2257f3_1601775540_extra_small.jpg&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231605#utm_campaign=onpage">
-                ពិធីសម្ពោធជាផ្លូវការនៃរថយន្ត Ford Everest 2.0L Bi-Turbo ជំនាន់ថ្មី
-              </a>
-            </div>
-          </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231053#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/Sophearom/Stress/5f787b45892fe_1601731380_extra_small.jpg" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/Sophearom/Stress/5f787b45892fe_1601731380_extra_small.jpg&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231053#utm_campaign=onpage">
-                ១០ចំណុចបង្ហាញថាអ្នកកំពុងគិតច្រើនជ្រុល អាចឈានទៅមានជំងឺផ្លូវចិត្ត និងដំណោះស្រាយ
-              </a>
-            </div>
-          </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231602#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/TECH-KK/SOVATH/Sep-2020-(3)/5f7922ea20c28_1601774280_extra_small.png" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/TECH-KK/SOVATH/Sep-2020-(3)/5f7922ea20c28_1601774280_extra_small.png&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231602#utm_campaign=onpage">
-                តើអ្នកជាអ្នកម្តាយដ៏ឆ្លាតវៃ ឬទេ? សូមផ្ទៀងផ្ទាត់មើលថាតើមានសារធាតុផ្សំប៉ុន្មានប្រភេទក្នុងទឹកដោះគោម្សៅរបស់កូនលោកអ្នក?
-              </a>
-            </div>
-          </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231596#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/sabay-news/Social-/CJ/Bin-Bunna/corona(7)/5f791e5780379_1601773140_extra_small.png" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/sabay-news/Social-/CJ/Bin-Bunna/corona(7)/5f791e5780379_1601773140_extra_small.png&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231596#utm_campaign=onpage">
-                មកដឹងពីស្ថានភាពជំងឺកូវីដ១៩ នៅព្រឹកថ្ងៃអាទិត្យនេះ
-              </a>
-            </div>
-          </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231593#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/sabay-news/Local-Entertainment/Another-124/5f7879f472515_1601731020_extra_small.jpg" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/sabay-news/Local-Entertainment/Another-124/5f7879f472515_1601731020_extra_small.jpg&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231593#utm_campaign=onpage">
-                ថា​មែន​ៗ ល្បី Tho Meet ធ្វើ​បទ​ពិសេស​ឲ្យ វី ឌីណែត ឥឡូវ​តម្រុយ​ចេញ​ច្បាស់​ក្រឡែត​តែ​ម្ដង!
-              </a>
-            </div>
-          </div>
-                                                                    <div class="media">
-            <div class="media-left right-small-post item">
-              <a href="https://news.sabay.com.kh/article/1231590#utm_campaign=onpage">
-                <div class="img lozad" data-background-image="https://cdn.sabay.com/cdn/media.sabay.com/media/TECH-KK/SOVATH/Sep-2020-(3)/5f783bd5deeab_1601715120_extra_small.jpg" data-loaded="true" style="background-image: url(&quot;https://cdn.sabay.com/cdn/media.sabay.com/media/TECH-KK/SOVATH/Sep-2020-(3)/5f783bd5deeab_1601715120_extra_small.jpg&quot;);"></div>
-              </a>
-            </div>
-            <div class="media-body">
-              <a href="https://news.sabay.com.kh/article/1231590#utm_campaign=onpage">
-                ព្រឹត្តិការណ៍ដ៏អស្ចារ្យ! ព្រះនាងតូច នរោត្តមជេនណា ក្លាយ​ជាទូតសុឆន្ទៈរបស់ក្រុមហ៊ុនអចលនទ្រព្យដ៏ធំមួយ
-              </a>
-            </div>
-          </div>
-                                                                        <div class="clear"></div>
+        @endforeach
+        <div class="clear"></div>
     </div>
   </div>
-      <div id="ad_zone_03" class="ads_items web" zone="9"><ins style="text-decoration: none;"><a href="https://ads.sabay.com/openx/www/delivery/ck.php?oaparams=2__bannerid=49221__zoneid=222__cb=980e86c772__oadest=https%3A%2F%2Fbit.ly%2F2UhO0gM" target="_blank"><img src="https://ads.sabay.com/images/ba7b70eedeb5efa71372c09e077d7956.jpg" width="300" height="250" alt="" title="" border="0"></a><div id="beacon_980e86c772" style="position: absolute; left: 0px; top: 0px; visibility: hidden;"><img src="https://ads.sabay.com/openx/www/delivery/lg.php?bannerid=49221&amp;campaignid=14206&amp;zoneid=222&amp;loc=https%3A%2F%2Fnews.sabay.com.kh%2Ftopics%2Ftechnology&amp;referer=https%3A%2F%2Fnews.sabay.com.kh%2Ftopics%2Fentertainment&amp;cb=980e86c772" width="0" height="0" alt="" style="width: 0px; height: 0px;"></div></ins><script async="" src="//ads.sabay.com/openx/www/delivery/asyncjs.php"></script></div>
-
-      </div>
+<div id="ad_zone_03" class="ads_items web" zone="9"><ins style="text-decoration: none;"><a href="https://ads.sabay.com/openx/www/delivery/ck.php?oaparams=2__bannerid=49221__zoneid=222__cb=980e86c772__oadest=https%3A%2F%2Fbit.ly%2F2UhO0gM" target="_blank"><img src="https://ads.sabay.com/images/ba7b70eedeb5efa71372c09e077d7956.jpg" width="300" height="250" alt="" title="" border="0"></a><div id="beacon_980e86c772" style="position: absolute; left: 0px; top: 0px; visibility: hidden;"><img src="https://ads.sabay.com/openx/www/delivery/lg.php?bannerid=49221&amp;campaignid=14206&amp;zoneid=222&amp;loc=https%3A%2F%2Fnews.sabay.com.kh%2Ftopics%2Ftechnology&amp;referer=https%3A%2F%2Fnews.sabay.com.kh%2Ftopics%2Fentertainment&amp;cb=980e86c772" width="0" height="0" alt="" style="width: 0px; height: 0px;"></div></ins><script async="" src="//ads.sabay.com/openx/www/delivery/asyncjs.php"></script></div></div>
