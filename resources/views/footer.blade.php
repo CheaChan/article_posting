1c3f92f0552a80b2e020c764075464cf6367264d
neamsinat <neamsinat8@gmail.com>
2020-10-04 22:45:22 +0700

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,37 +1,69 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Article Post</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.css">
+        <!-- Styles -->
+        <style>
+           @media only screen and (min-width: 29.8125rem /* 477px */) {
+  .footer-bottom {
+    justify-content: space-evenly;
+  }
+}
+@media only screen and (min-width: 77.5rem /* 1240px */) {
+  .footer-bottom {
+    justify-content: space-evenly;
+  }
+}
+        </style>
+        <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custome.css') }}" rel="stylesheet">
+    </head>
+    <body>
  <!-- Footer -->
  <footer style="position: static;
   left: 0;
   bottom: 0;">
-    <div class="footer-bottom" style="background-color:#343a40; color:white; position: absolute; bottom: -280%;  width: 100%; height: 400px; line-height: 100%;">
+    <div class="footer-bottom" style="background-color:#343a40; color:white; position: absolute; bottom: -240%;  width: 100%; height: 400px; line-height: 100%;">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-lg-4 footer-about wow fadeInUp">
+                <div class="col-md-4 col-lg-4 col-sm-4 footer-about wow fadeInUp">
                     <img src="http://127.0.0.1:8000/images/uploads/file/image-default.png" class="img-fluid image-resolution" alt="Responsive image">
                     <br> <br>
                     <p><i class="fas fa-map-marker-alt"></i> BP511 St. 371 Phum Tropeang Chhuk (Borey Sorla), Sangkat Tek Thla, Khan Sen Sok, Phnom Penh</p>
                     <p><i class="fas fa-envelope"></i> Email: <a  class="text-white" href="#">articleposting@gmail.com</a></p>
                     <p><i class="fas fa-phone"></i> Phone: 097 46 64 594</p>
                 </div>
-                <div class="col-md-4 col-lg-4 offset-lg-1 footer-contact wow fadeInDown">
+                <div class="col-md-4 col-lg-4 col-sm-4 offset-lg-1 footer-contact wow fadeInUp">
                     <h4>ទំនាក់ទំនងមកកាន់ Article posting</h4>
                     <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg>  ផ្នែកនិពន្ធ (+855) 97 46 64 594</p>
+                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                      </svg>  ផ្នែកនិពន្ធ (+855) 97 46 64 594</p>
                     <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg></i> ផ្នែកផ្សព្វផ្សាយ (+855) 97 46 64 594</p>
+                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                      </svg></i> ផ្នែកផ្សព្វផ្សាយ (+855) 97 46 64 594</p>
                     <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg> ផ្នែកសៀវភៅ (+855) 97 46 64 594</p>
+                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                  </svg> ផ្នែកសៀវភៅ (+855) 97 46 64 594</p>
                     <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg></i> Facebook: Article Posting</p>
-<p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-  </svg></i> Telegram: (+855) 97 46 64 594</p>
+                      <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                    </svg></i> Facebook: Article Posting</p>
+                    <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                      </svg></i> Telegram: (+855) 97 46 64 594</p>
                 </div>
-                <div class="col-md-4 col-lg-3 footer-social wow fadeInUp">
+                <div class="col-md-4 col-lg-3 col-sm-4 footer-social wow fadeInUp">
                     <h4> Popular Category</h4>
                     <p><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
@@ -48,5 +80,6 @@
                 </div>
             </div>
         </div>
-    </div>
-</footer>
+    </div></footer>
+    </body>
+</html>
