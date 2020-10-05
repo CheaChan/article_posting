4b0cca4e393ac4cfdbf277949e0296215f4a032a
cheadevit <ch-devit@global-mobility-service.com>
2020-10-05 11:45:46 +0700

    Api configure and end-point

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -17,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password'
+        'name', 'email', 'password', 'f_name', 'l_name'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -14,60 +14,66 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', 'Api\PassportAuthController@register');
+Route::post('login', 'Api\PassportAuthController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Article list
-Route::get('article/list/{category_id?}', function ($category_id = '')
-{
-    if ($category_id == '' || $category_id == null) {
-        $data = Article::where('status', '1')->paginate(10);
-    }
-    if ($category_id != '' || $category_id != null) {
-        $data = Article::where('status', '1')->where('category_id', $category_id)->paginate(10);
-    }
+Route::group(['prefix' => 'v0', 'middleware' => 'auth:api'], function () {
+    // Article list
+    Route::get('article/list/{category_id?}', function ($category_id = '')
+    {
+        if ($category_id == '' || $category_id == null) {
+            $data = Article::where('status', '1')->paginate(10);
+        }
+        if ($category_id != '' || $category_id != null) {
+            $data = Article::where('status', '1')->where('category_id', $category_id)->paginate(10);
+        }
 
-    return $data;
+        return $data;
+    });
+
+    // Article detail
+
+    Route::get('article/detail/{article_id?}', function ($article_id = '')
+    {
+        $article_detail = Article::where('id', $article_id)->select('id')->first();
+
+        if ($article_id == null || $article_id == '' && $article_detail === null || $article_detail === '') {
+            $response = ['response' => false, 'message' => 'Not Found'];
+        }
+
+        if ($article_id != '' || $article_id != null && $article_detail->id != null) {
+            $data = Article::where('status', '1')->where('id', $article_id)->with('articlePhotoHasOne')->first();
+
+            $response = ['response' => true, 'message' => 'Successfully', 'data' => $data];
+        }
+
+        return $response;
+    });
+
+
+    // Search
+
+    Route::get( 'article/search', function (Request $request) {
+        $article_list = Article::where ( 'title', 'LIKE', '%' . $request->title . '%' )->paginate(10);
+        if (count( $article_list ) > 0)
+            return ['response' => true, 'message' => 'Successfully', 'data' => $article_list];
+        else
+            return ['response' => false, 'message' => 'Not Found'];
+    } );
+
+
+    // Filter
+    Route::get( 'article/filter', function (Request $request) {
+        $article_list = Article::whereBetween( 'published_at', [$request->start_date, $request->end_date])->paginate(10);
+        if (count( $article_list ) > 0)
+            return ['response' => true, 'message' => 'Successfully', 'data' => $article_list];
+        else
+            return ['response' => false, 'message' => 'Not Found'];
+    });
+
 });
 
-// Article detail
-
-Route::get('article/detail/{article_id?}', function ($article_id = '')
-{
-    $article_detail = Article::where('id', $article_id)->select('id')->first();
-
-    if ($article_id == null || $article_id == '' && $article_detail === null || $article_detail === '') {
-        $response = ['response' => false, 'message' => 'Not Found'];
-    }
-
-    if ($article_id != '' || $article_id != null && $article_detail->id != null) {
-        $data = Article::where('status', '1')->where('id', $article_id)->with('articlePhotoHasOne')->first();
-
-        $response = ['response' => true, 'message' => 'Successfully', 'data' => $data];
-    }
-
-    return $response;
-});
-
-
-// Search
-
-Route::get( 'article/search', function (Request $request) {
-    $article_list = Article::where ( 'title', 'LIKE', '%' . $request->title . '%' )->paginate(10);
-    if (count( $article_list ) > 0)
-        return ['response' => true, 'message' => 'Successfully', 'data' => $article_list];
-    else
-        return ['response' => false, 'message' => 'Not Found'];
-} );
-
-
-// Filter
-Route::get( 'article/filter', function (Request $request) {
-    $article_list = Article::whereBetween( 'published_at', [$request->start_date, $request->end_date])->paginate(10);
-    if (count( $article_list ) > 0)
-        return ['response' => true, 'message' => 'Successfully', 'data' => $article_list];
-    else
-        return ['response' => false, 'message' => 'Not Found'];
-} );
